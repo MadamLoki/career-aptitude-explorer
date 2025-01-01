6246dd21a9be714886248f98f98b4e55dc0765d7
William Sheere IV <Sheerebilly@yahoo.com>
2025-01-01 17:22:16 -0500

    added starter files, fixed react deploy, added header

---
+++ frontend/Aspire/src/components/Header.tsx
@@ -0,0 +1,24 @@
+const Header = () => {
+	return (
+		<header className="header">
+			<div className="container">
+				<h1>Career Aptitude Explorer</h1>
+				<nav>
+					<ul className="nav-links">
+						<li>
+							<a href="#features">Features</a>
+						</li>
+						<li>
+							<a href="#testimonials">Testimonials</a>
+						</li>
+						<li>
+							<a href="#cta">Get Started</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
