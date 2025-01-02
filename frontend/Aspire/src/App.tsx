065235b9fc86e45ab18cadea9a1d92fded969638
Sara <lokifeathers@gmail.com>
2025-01-02 18:54:08 -0500

    main and app tsx framework

--- frontend/Aspire/src/App.tsx
+++ frontend/Aspire/src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
 function App() {
 	return (
 		<>
-  <Header/>
+			<Header />
+
+			<Outlet />
+
+			<Footer />
 		</>
 	);
 }
