3af7aeee2748f1851b06113d8d04b75391bc7fa9
Adebanjo Fajemisin <marcusfajemisin@gmail.com>
2025-01-07 19:17:34 -0500

    removed repeated files and code

--- backend/src/config/server.ts
+++ backend/src/config/server.ts
@@ -1,2 +1,16 @@
+import express from 'express';
+import sequelize from 'backend\src\config\server.ts'
+import routes from './routes';
+const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+// Force true to drop/recreate table(s) on every sync
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+});
