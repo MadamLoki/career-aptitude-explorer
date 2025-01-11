8746dce97b10878d69ff8303a1906f26e5a76f5f
Sara <lokifeathers@gmail.com>
2025-01-10 22:01:21 -0500

    pathing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 
 // Serve static files from frontend build
-const frontendPath = path.join(__dirname, '../../../frontend/dist');
+const frontendPath = path.join(__dirname, '../../../frontend/dist/index.html');
 console.log('Frontend path:', frontendPath);
 app.use(express.static(frontendPath));
 
