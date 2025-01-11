fc693f2a164580c24dc39a91122bae7a8fa24ead
William Sheere IV <Sheerebilly@yahoo.com>
2025-01-11 16:02:19 -0500

    styling kata

--- frontend/src/pages/Skills.tsx
+++ frontend/src/pages/Skills.tsx
@@ -31,7 +31,7 @@ function Skills() {
 					{kata ? (
 						<div className="text-center text-gray-400">
 							<h1 className="text-4xl font-bold">Random Daily Kata</h1>
-							<div className="my-14 text-xl">{kata.name}</div>
+							<div className="my-14 text-xl min-h-[30px]">{kata.name}</div>
 							<div>
 								<a href={kata.url} target="_blank" rel="noopener noreferrer">
 									<button className="hover:shadow-lg bg-teal-500/50 font-semibold border-teal-400 py-3 px-6">
